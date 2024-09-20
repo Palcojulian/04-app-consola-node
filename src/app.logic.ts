77bde86029055a19614e577389bab6f3c8f4f6a8
Julian Palco <palcojulian@outlook.com>
2024-09-19 22:53:52 -0500

    Required vars from args with yarg

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,12 +1,13 @@
 import { writeFileSync, mkdirSync } from 'fs';
+import { yarg } from './config/plugins/yargs.plugin';
 
-const n: number = 10;  
+const { b: base, l: limit, s: showTable, t: saveFicheroTxt } = yarg;
 
 let text:string = ''
 
 for (let index = 1; index <= 3; index++) {
     if (index % 2 === 0) {
-        text += '         Tabla del 5         \n';
+        text += `         Tabla del ${base}         \n`;
     } else {
         for (let j = 0; j < 30; j++) {
             text += "=";
@@ -15,12 +16,18 @@ for (let index = 1; index <= 3; index++) {
     }
 }
 
-for (let index = 1; index <= n; index++) {
-    const resul = `5 x ${index} = ${5 * index} \n`;
+for (let index = 1; index <= limit; index++) {
+    const resul = `${base} x ${index} = ${base * index} \n`;
     text += resul;
 }
 
-const outputPath = 'outputs/multiplicacion';
-mkdirSync(`${outputPath}`, { recursive: true });
-writeFileSync(`${outputPath}/tabla-${5}.txt`, text);
-console.log('File created');
+if (showTable) console.log(text);
+
+if (saveFicheroTxt) {
+    const outputPath = 'outputs/multiplicacion';
+    mkdirSync(`${outputPath}`, { recursive: true });
+    writeFileSync(`${outputPath}/tabla-${base}.txt`, text);
+    console.log('File created');
+}
+
+console.log('End program :3 ');
