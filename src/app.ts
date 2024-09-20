d5bd25e595dc740b108ee1d9d0b9680a39a8874a
Julian Palco <palcojulian@outlook.com>
2024-09-19 22:54:23 -0500

    begin implemetation clean implementation

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 
-import { yarg  as argv } from "./config/plugins/yargs.plugin";
+import { yarg  as argv, yarg } from "./config/plugins/yargs.plugin";
+import { ServerApp } from "./presentation/server-app";
+
 
 // console.log(process.argv);
 // console.log(argv);
 
 (async() => {
     await main();
-    console.log('Fin del programa');
-
 })();
 
 async function main() {
-    console.log('Main ejecutado');
-    
+    const { b: base, l: limit, s: showTable, t:saveFicheroTxt } = argv;
+
+    ServerApp.run({ base, limit, showTable, saveFicheroTxt,});
 }
