e26e4ec92f674af1125df308082ac8334d022d48
Julian Palco <palcojulian@outlook.com>
2024-09-21 10:42:34 -0500

    import useCases

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,15 +1,30 @@
+import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
+
 interface RunOptions {
-    base: number;
-    limit: number;
-    showTable: boolean;
-    saveFicheroTxt: boolean;
+    base            : number;
+    limit           : number;
+    showTable       : boolean;
+    saveFicheroTxt  : boolean;
+    name            : string;
+    destination     : string;
 }
 
 
 export class ServerApp {
 
-    static run(options: RunOptions) {
-        console.log(options);
+    static run({ base, limit, saveFicheroTxt, showTable, destination, name }: RunOptions) {
         console.log('Server runnig...');
+        console.log({ base, limit, saveFicheroTxt, showTable });
+
+        const table = new CreateTable().execute({ base, limit });
+        const wasCreatedFile = new SaveFile().execute({
+            fileContent: table,
+            fileDestination: destination,
+            fileName: name,
+        });
+
+        if (showTable) console.log(table);
+
     }
 }
