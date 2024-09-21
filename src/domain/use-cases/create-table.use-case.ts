0ecb0c3215bd05be5e69cdd618264d62243365ec
Julian Palco <palcojulian@outlook.com>
2024-09-21 10:40:55 -0500

    use case create table

---
+++ src/domain/use-cases/create-table.use-case.ts
@@ -0,0 +1,27 @@
+export interface CreateTableUseCase {
+    execute: (options: Options) => string;
+}
+
+export interface Options {
+    base: number;
+    limit?: number;
+}
+
+export class CreateTable  implements CreateTableUseCase{
+
+    constructor(
+        /**
+       * DI - Dependency Injection
+       */
+    ) { };
+
+    execute({ base, limit = 10 } : Options) {
+
+        let outputMessage: string = '';
+        for (let index = 1; index <= limit; index++) {
+            outputMessage  += `${base} x ${index} = ${base * index} \n`;
+        }
+
+        return outputMessage;
+    }
+}
