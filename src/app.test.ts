6e032879ed853294db4863268485b6bd3dce7427
Julian Palco <palcojulian@outlook.com>
2024-10-04 05:42:08 -0500

    Implementa test app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,21 @@
-describe('App', () => {
-    test('Should be true', () => {
-        expect(true).toBe(true);
+import { ServerApp } from './presentation/server-app'
+
+describe('Test App.ts', () => {
+    test('Should call Server.run with values', async () => {
+        const serverRunMock = jest.fn();
+        ServerApp.run = serverRunMock;
+
+        process.argv = ['node', 'app.ts', '-b', '10', '-l', '6', '-s'];
+
+        await import('./app');
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            "base": 10,
+            "limit": 6,
+            "destination": "outputs",
+            "name": "multiplication-table",
+            "saveFicheroTxt": false,
+            "showTable": true,
+        })
     })
 })
